66f81875224798bd5f8bb2c356691db095916992
DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>
2025-04-08 11:40:13 +0300

    format other documents
    Signed-off-by: DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>

--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
@@ -50,7 +50,7 @@ function startMover($options = "start")
         $ioLevel = $cfg['moverIO'] ?: "-c 2 -n 0";
 
         if ($cfg['moveThreshold'] >= 0 or $cfg['age'] == "yes" or $cfg['sizef'] == "yes" or $cfg['sparsnessf'] == "yes" or $cfg['filelistf'] == "yes" or $cfg['filetypesf'] == "yes" or $cfg['$beforescript'] != '' or $cfg['$afterscript'] != '' or $cfg['testmode'] == "yes") {
-			$age_mover_str = "/usr/local/emhttp/plugins/ca.mover.tuning/age_mover start";
+            $age_mover_str = "/usr/local/emhttp/plugins/ca.mover.tuning/age_mover start";
             //exec("echo 'about to hit mover string here: $age_mover_str' >> /var/log/syslog");
             logger("ionice $ioLevel nice -n $niceLevel $age_mover_str");
             passthru("ionice $ioLevel nice -n $niceLevel $age_mover_str");
@@ -63,7 +63,6 @@ function startMover($options = "start")
         logger("ionice $ioLevel nice -n $niceLevel /usr/local/sbin/mover.old $options");
         passthru("ionice $ioLevel nice -n $niceLevel /usr/local/sbin/mover.old $options");
     }
-
 }
 
 if ($argv[2]) {
