2202631425fcf3442cbd397f6bc709e027e30ba5
DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>
2025-05-03 12:48:55 +0300

    fix for disable/enable mover on schedule
    Signed-off-by: DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>

--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/updateCron.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/updateCron.php
@@ -1,10 +1,25 @@
 #!/usr/bin/php
 <?PHP
+
 if ($_POST['cronEnabled'] == "yes") {
-	$cronFile = "# Generated schedule for forced move\n" . trim($_POST['cron']) . " /usr/local/sbin/mover.old start |& logger -t move\n\n";	
+	$cronFile = "# Generated schedule for forced move\n" . trim($_POST['cron']) . " /usr/local/sbin/mover.old start |& logger -t move\n\n";
 	file_put_contents("/boot/config/plugins/ca.mover.tuning/mover.cron", $cronFile);
 } else {
 	@unlink("/boot/config/plugins/ca.mover.tuning/mover.cron");
 }
+
+# if mover schedule is disabled then rename 'mover.cron' to 'mover.cron.disabled'
+if ($_POST['ismoverDisabled'] == "yes") {
+	// Check if the file exists before attempting to rename it
+	if (file_exists("/boot/config/plugins/dynamix/mover.cron")) {
+		rename("/boot/config/plugins/dynamix/mover.cron", "/boot/config/plugins/dynamix/mover.cron.disabled");
+	}
+} else {
+	# if mover schedule is enabled then rename back
+	if (file_exists("/boot/config/plugins/dynamix/mover.cron.disabled")) {
+		rename("/boot/config/plugins/dynamix/mover.cron.disabled", "/boot/config/plugins/dynamix/mover.cron");
+	}
+}
+
 exec("update_cron");
 ?>
