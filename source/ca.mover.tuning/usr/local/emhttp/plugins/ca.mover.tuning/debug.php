1e3a18bf373ead5fddab13e5a931c670f9dace84
DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>
2025-11-21 11:30:47 +0200

    back exec in debug
    Signed-off-by: DaRK AnGeL <28630321+masterwishx@users.noreply.github.com>

--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/debug.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/debug.php
@@ -1,7 +1,6 @@
 <?PHP
 
-$cmd = escapeshellcmd("/usr/bin/timeout -s9 90 /usr/local/emhttp/plugins/ca.mover.tuning/debug_mover 2>&1");
-exec($cmd, $debug_result, $exit_code);
+exec("/usr/bin/timeout -s9 90 /usr/local/emhttp/plugins/ca.mover.tuning/debug_mover 2>&1", $debug_result, $exit_code);
 
 if(!empty($debug_result)) {
     if(strpos(end($debug_result), "DONE:") !== false) {
