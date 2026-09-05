458f4f60ad068fd83125a4ade286e230a4c9e0a8
chodeus <dhoges90@outlook.com>
2026-09-05 12:49:05 +0800

    Remove dead code, fix config key names, correct README test-mode note
    getShareSettings() has had no caller since 32763b7; mover.php read
    beforescript/afterscript while the config keys are beforeScript and
    afterScript; moveNow() in Mover.tuning.page is unused; the README still
    said Test Mode is on after installation.

--- source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
+++ source/ca.mover.tuning/usr/local/emhttp/plugins/ca.mover.tuning/mover.php
@@ -173,7 +173,7 @@ function startMover()
     if ($cron or $cfg['movenow'] == "yes") {
         //exec("echo 'running from cron or move now question is yes' >> /var/log/syslog");
 
-        if ($cfg['movingThreshold'] >= 0 or $cfg['fillupThreshold'] >= 0 or $cfg['age'] == "yes" or $cfg['sizef'] == "yes" or $cfg['sparsnessf'] == "yes" or $cfg['filelistf'] == "yes" or $cfg['filetypesf'] == "yes" or $cfg['beforescript'] != '' or $cfg['afterscript'] != '' or $cfg['testmode'] == "yes") {
+        if ($cfg['movingThreshold'] >= 0 or $cfg['fillupThreshold'] >= 0 or $cfg['age'] == "yes" or $cfg['sizef'] == "yes" or $cfg['sparsnessf'] == "yes" or $cfg['filelistf'] == "yes" or $cfg['filetypesf'] == "yes" or $cfg['beforeScript'] != '' or $cfg['afterScript'] != '' or $cfg['testmode'] == "yes") {
             $age_mover_str = "/usr/local/emhttp/plugins/ca.mover.tuning/age_mover";
             //exec("echo 'about to hit mover string here: $age_mover_str' >> /var/log/syslog");
             logger("ionice $ioLevel nice -n $niceLevel $age_mover_str $options");
